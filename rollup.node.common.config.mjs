849aed5a72048b63d37c18afbe0398f4fd394bdc
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2024-06-18 21:05:51 +0200

    update build

--- rollup.node.common.config.mjs
+++ rollup.node.common.config.mjs
@@ -16,9 +16,11 @@ for (const mod of nonPrefixedBuiltinModules) {
 }
 const builtinModules = new Set([...nonPrefixedBuiltinModules, ...prefixedBuiltinModules])
 
-const isCjsRegex = /(((module)((\[[`"'])|\.))?exports([`"']\])?((\.|(\[[`"']))[^\s\n"`']+([`"']\])?)?\s*=\s*[^\s\n]+)|require\(["'`][^\s\n"'`]+["'`]\)/gm
+const isCjsRegex = /(((module)((\[[`"'])|\.))?exports([`"']\])?((\.|(\[[`"']))[^\s\n"`']+([`"']\])?)?\s*=\s*[^\s\n]+)|require\(["'`][^\s\n"'`]+["'`]\)/m
 
-
+// todo propagate updates to other modules that use this
+// onetime is still bugged
+// open issue in rollup
 
 const config = {
   input: './repl/src/repl.ts',
@@ -32,8 +34,12 @@ const config = {
   plugins: [
     typescript({tsconfig: "./tsconfig.json", noEmitOnError: false, sourceMap: false}), 
     // these resolve options do not matter if resolveOnly is used. ModulesOnly does currently not prefer cjs exports if they exists, instead it finds the first esm version and transpiles it, which we want to avoid if we can.
-    resolve({modulesOnly: true, preferBuiltins: true, resolveOnly(mod) {
+    resolve({modulesOnly: false, preferBuiltins: true, resolveOnly(mod) {
+      // if (mod !== "onetime") return true
+      // memoize? What about different major version that are required from different modules. This is an edge case anyways
+
       if (builtinModules.has(mod)) return false
+        
       // this can happen when a module defines custom imports (e.g. chalk) in its package.json. We have no way tho to know here from where it was imported, so we just assume it was imported from a esm module and that it is esm as well. The only case that is known to not be covered here is when this module itself uses custom imports in package.json, to include this we would have to check if the given module is in local in this package. But I don't think that is worth the effort.
       if (!fs.existsSync(path.join("node_modules", mod))) {
         console.warn("may be resolved sub module:", mod)
@@ -42,8 +48,9 @@ const config = {
 
       const json = JSON.parse(fs.readFileSync(path.join("node_modules", mod, "package.json"), "utf8"))
 
+
       if (json.type === "module") {
-        console.error("resolved module:", mod)
+        console.error("resolved module1:", mod)
         return true
       }
 
@@ -70,27 +77,41 @@ const config = {
         "main"
       ])
 
-      for (let val of vals) {
-        if (typeof val !== "string") continue
-        if (val.endsWith(".mjs")) continue
-        if (fs.existsSync(path.join("node_modules", mod, val))) {
-          if (fs.statSync(path.join("node_modules", mod, val)).isDirectory()) val = path.join(val, "index.js")
-          else if (!val.endsWith(".js")) val = val + ".js"
-          if (!fs.existsSync(path.join("node_modules", mod, val))) continue
-        }
-        if (!val.endsWith(".js")) continue
+      vals.push("index.js")
 
-        const fileContent = fs.readFileSync(path.join("node_modules", mod, val), "utf8")
-        const isCjs = isCjsRegex.test(fileContent)
+
+      for (let val of vals) {
+        const valS = val instanceof Array ? val : [val]
+        for (const val of valS) {
+          if (typeof val !== "string") continue
+          if (val.endsWith(".mjs")) continue
+          if (fs.existsSync(path.join("node_modules", mod, val))) {
+            if (fs.statSync(path.join("node_modules", mod, val)).isDirectory()) val = path.join(val, "index.js") 
+          }
+          if (!val.endsWith(".js")) val = val + ".js"
+          if (!fs.existsSync(path.join("node_modules", mod, val))) continue
+          
+
+          const fileContent = fs.readFileSync(path.join("node_modules", mod, val), "utf8")
+          const isCjs = isCjsRegex.test(fileContent)
+          
+          if (isCjs) {
+            console.log(fileContent)
+            console.log("resolved module2:", mod, "as cjs")
+            return false
+          }
+        }
         
-        
-        if (isCjs) return false
       }
 
-      console.error("resolved module:", mod)
+      console.error("resolved module2:", mod)
 
       return true
-    }}),
+    }
+
+      
+      
+    }),
     // do i even need this?
     commonJS({
       include: 'node_modules/**'
